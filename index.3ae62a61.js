a3c8deebdf2465e52e1376fc76f9ef59fcdfef2f
Yurii Kramar <y.kramar15@gmail.com>
2024-08-18 19:26:00 +0200

    make build

---
+++ index.3ae62a61.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,Object.keys(t[a]).length>0&&e(l,t[a]),r.appendChild(l)}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.3ae62a61.js.map
